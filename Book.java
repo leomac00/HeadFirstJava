45a0b5656a2492e7ceacd82de8787b0c70afead9
lomh <leonardo.machado@gft.com>
2023-01-03 21:52:13 -0300

    Chapter 3 complete

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book {
+  public String title;
+
+  public Book() {
+    super();
+  }
+
+  public Book(String title) {
+    this.title = title;
+  }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    // Heap showcase
+    Book a = new Book("AAA");
+    Book b = new Book("BBB");
+    Book c;
+    c = a;
+    Book d = new Book("DDD");
+    d = null;
+    System.out.println("Book 'a'´s Title: " + a.title);
+    System.out.println("Book 'b'´s Title: " + b.title);
+    System.out.println("Book 'c'´s Title: " + c.title);
+    // System.out.println("Book 'd'´s Title: " + d.Title);
+
+    // Array
+    Book[] books = new Book[3];
+    books[0] = new Book("book[0]");
+    books[1] = new Book("book[1]");
+    books[2] = books[0];
+
+    System.out.println("Book[0]'s title: " + books[0].title);
+    System.out.println("Book[1]'s title: " + books[1].title);
+    System.out.println("Book[2]'s title: " + books[2].title);
+
   }
 }
