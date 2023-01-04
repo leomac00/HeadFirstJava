07fe326f29f44d7019a0f735ed575de3e36d7d61
lomh <leonardo.machado@gft.com>
2023-01-04 10:38:28 -0300

    Chapter 4 complete

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
+  static int calcArea(int a, int b) {
+    return a * b;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    short a = 12;
+    int result = calcArea(a, 2);
+    System.out.println(result);
   }
 }
