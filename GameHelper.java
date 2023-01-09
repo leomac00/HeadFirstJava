19f8507956853c062f5c1dcedcedc4042399f0f1
lomh <leonardo.machado@gft.com>
2023-01-09 17:44:59 -0300

    Changing Scanner() to Console()

--- GameHelper.java
+++ GameHelper.java
@@ -14,9 +14,7 @@ public class GameHelper {
 
   public String getUserInput(String prompt) {
     System.out.print(prompt + ": ");
-    Scanner scanner = new Scanner(System.in);
-    String text = scanner.nextLine().toLowerCase();
-    scanner.close();
+    String text = System.console().readLine();
     return text;
   }
 
