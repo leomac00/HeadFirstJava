63b14cfbd10cd0ccf762e89e48cf8a1bf12e1c79
lomh <leonardo.machado@gft.com>
2023-01-23 15:04:17 -0300

    Chapter 09 complete - Constructors

--- Animal.java
+++ Animal.java
@@ -2,20 +2,20 @@ public abstract class Animal {
   String name;
   int size;
 
-  public int getSize() {
-    return size;
-  }
-
-  public void setSize(int size) {
-    this.size = size;
-  }
-
   public String getName() {
     return name;
   }
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
   }
 
   public Animal() {
